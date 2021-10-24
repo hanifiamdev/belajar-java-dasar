49736ffe516c0daab01728a184c2f37d30c9b77a
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-24 21:19:42 +0700

    Materi Operasi Perbandingan

---
+++ src/OperasiPerbandingan.java
@@ -0,0 +1,22 @@
+public class OperasiPerbandingan {
+    public static void main(String[] args) {
+        /*
+        * Operasi perbandingan adalah operasi yang membandingkan dua buah data dengan menghasilkan nilai true jika benar dan false jika salah
+        * > / Lebih Dari
+        * < / Kurang Dari
+        * >= / Lebih Dari Sama Dengan
+        * <= / Kurang Dari Sma Dengan
+        * == / Sama Dengan
+        * != / Tidak Samam Dengan
+        * */
+        int a = 100;
+        int b = 100;
+
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
+    }
+}
