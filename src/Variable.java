780360aef000194b525474bbce2d105e4a0ee7bd
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-24 14:34:20 +0700

    Materi Variable

--- src/Variable.java
+++ src/Variable.java
@@ -12,5 +12,22 @@ public class Variable {
         System.out.println(age);
         System.out.println(address);
 
+        /* sejak java versi 10, java mendukung kata kunci variabel dengan kata kunci var, sehingga kita tidak perlu
+        *  menyebutkan tipe datanya. Namun perlu diingat untuk menggunakan variabel dengan kata kunci var kita harus
+        * menginisialisasi value/nilai tersebut secara langsung*/
+        var firstName = "Fachri";
+        var middleName = "Mufti";
+        var lastName = "Amrullah";
+        // aKAN ERROr J ika deklarasi variabel var tanpa memberikan value secara langsung
+        /*var company;
+        company = "Terlkom";*/
+
+        /* final atau variabel konstan yang nilai tidak bisa diubah*/
+        final String application = "Belajar Java";
+
+        // application = "PHP";  // ini erro karena mencobah merubah variabel konstan
+
+
+
     }
 }
