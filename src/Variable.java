2531ab6194595699c3355a91f3fa3a655149b2cd
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-24 14:17:59 +0700

    Materi Variable

---
+++ src/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+
+    public static void main(String[] args) {
+
+        String name;
+        name = "Hanif Amrullah";
+
+        System.out.println(name);
+        int age = 30;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+    }
+}
