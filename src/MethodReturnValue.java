6eaefc818f2b5ae9d3d8e8e586868c5b34a96eb3
hanifiamdev <hanifamrullahdev@gmail.com>
2021-10-25 17:03:51 +0700

    Materi Method return Value

---
+++ src/MethodReturnValue.java
@@ -0,0 +1,33 @@
+public class MethodReturnValue {
+    /* Secara default, method tidak menghasilkan value apapun, namun jika diperlukan untuk membuat method mengembalikan nilai adalah dengan mnegubah kata kunci void dengan tipe data yanng dihasilkan
+     Dan didalam block method, untuk menghasilkan nilai tersebut, kita harus menggunakan kata kunci return, lalu diikuti dengan tipe data yang sudah kita deklarasikan di method
+     Di Java kita hanya bisa menghasilkan 1 data disebuah method. tidak bisa lebih dari satu*/
+    public static void main(String[] args) {
+        var result1 = sum(100, 100);
+        System.out.println(result1);
+
+        System.out.println(sum(200, 200));
+
+        System.out.println(hitung(100, "+", 100));
+        System.out.println(hitung(200, "-", 100));
+        System.out.println(hitung(100, "/", 100));
+
+
+    }
+
+    static int sum(int value1, int value2) {
+        var total = value1 + value2;
+        return total;
+    }
+
+    static int hitung(int value1, String operasi, int value2) {
+        switch (operasi) {
+            case "+":
+                return value1 + value2;
+            case "-":
+                return value1 - value2;
+            default:
+                return 0;
+        }
+    }
+}
