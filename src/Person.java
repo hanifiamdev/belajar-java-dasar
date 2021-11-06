05a5ec18be0eb7dff8b1585a12b0f255f9f6ff51
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-06 14:57:50 +0700

    Field

--- src/Person.java
+++ src/Person.java
@@ -2,7 +2,15 @@
 * ## Membuat Class ##
 * Untuk membuat class, kita bisa menggunakan kata kunci class
 * Penamaan class biasa menggunakan format CamelCase
+*
+* Field
+* Fields / Properties / Attributes adalah data yang bisa kita sisipkan di dalam Object
+* Namun sebelum kita bisa memasukkan data di fields, kita harus mendeklarasikan data apa aja yang dimiliki object tersebut di dalam deklarasi class-nya
+* Membuat field sama seperti membuat variable, namaun ditempatkan di block class
 * */
 
 public class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -5,13 +5,30 @@ public class PersonApp {
         Object adalah hasil instansiasi dari sebuah class
         UNtuk membuat object kita bisa menggunakan kata kunci new, dan diikuti dengan nama class dan kurung ()*/
         var person1 = new Person();
+
+       /* Manipulasi Field
+
+        Fields yang ada di object, bisa kita manipulasi. Tergantung final atau bukan
+        Jika final, berarti kita tidak bisa mengubah data fieldnya, namun jika tidak, kita bisa mengubah fieldnya
+        Untuk memanipulasi data field, sama seperti cara pada variable
+        Untuk mengakses field, kita butuh kata kunci .(titik) setelah nama object dan diikuti nma fieldsnya*/
+
+        person1.name = "Hanif";
+        person1.address = "Pemalang";
+        // person1.country = "Indonesia"; // field final jadi tidak boleh diubah
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
+
+
+
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
+
     }
 }
 
