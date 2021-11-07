4daf86b7c5d489e4050b95a6e4a4400fa53557a5
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-07 13:31:14 +0700

    Object Class

--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -6,5 +6,10 @@ public class ManagerApp {
 
         var vp = new VicePresident("Orkhan");
         vp.sayHello("Adiba");
+
+        System.out.println(manager);
+        System.out.println(manager.toString());
+        System.out.println(vp);
+        System.out.println(vp.toString());
     }
 }
