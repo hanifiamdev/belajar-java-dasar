b1d5c2436b18290f8441c37377f38c5c1e56b203
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-08 01:12:38 +0700

    toString() Method

--- src/hanifiamdev/com/data/ProductApp.java
+++ src/hanifiamdev/com/data/ProductApp.java
@@ -6,5 +6,7 @@ public class ProductApp {
         Product product = new Product("Mac Book Pro", 3000000);
         System.out.println(product.name); //protected sehingga masih bisa karena dalam 1 paket
         System.out.println(product.price);
+
+        System.out.println(product);
     }
 }
