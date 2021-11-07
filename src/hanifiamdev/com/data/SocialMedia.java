3c5167cc197f2ba83e22c894cd4075e09c574ac9
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-08 02:39:10 +0700

    Final Class

---
+++ src/hanifiamdev/com/data/SocialMedia.java
@@ -0,0 +1,11 @@
+package hanifiamdev.com.data;
+
+class SocialMedia {
+    String name;
+}
+
+final class Facebook extends SocialMedia {
+
+}
+
+//class FakeFacebook extends Facebook {}/// Tidak bisa di uanah lagi untuk class Facebook
