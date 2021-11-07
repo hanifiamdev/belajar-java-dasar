a756bf62aea5d9269b6398e13c5f047ea065b6bf
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-07 14:40:09 +0700

    Type Check dan Cast

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -16,11 +16,20 @@ public class PolymorphismApp {
         sayHello(new Manager("Joko"));
         sayHello(new VicePresident("Budi"));
 
+
     }
 
     // dengan satu method sayHello dan memasukkan sebuah parameter parentnya maka bisa digunakan dengan obejct dari turunannya ini juga konsep polymorphism
     // tanpa polymorphism kita harus membuat object sayHello tiga kali jika ingin digunakan oleh 3 object berbeda
-    static void sayHello(Employee employee){
-        System.out.println("Hello " + employee.name);
+    static void sayHello(Employee employee) {
+        if (employee instanceof VicePresident) {
+            VicePresident vicePresident = (VicePresident) employee;
+            System.out.println("Helloo VP " + vicePresident.name);
+        } else if (employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        } else {
+            System.out.println("Hello " +  employee.name);
+        }
     }
 }
