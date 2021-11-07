56a83145235f333443c3b63a9c9917c1109739df
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-08 02:25:12 +0700

    hashCode() Method

--- src/hanifiamdev/com/data/ProductApp.java
+++ src/hanifiamdev/com/data/ProductApp.java
@@ -11,5 +11,6 @@ public class ProductApp {
 
         Product product2 = new Product("Mac Book Pro", 3000000);
         System.out.println(product.equals(product2)); // true karena kita sudah override method equals dan isi di parameternya sama
+        System.out.println(product.hashCode() == product2.hashCode());
     }
 }
