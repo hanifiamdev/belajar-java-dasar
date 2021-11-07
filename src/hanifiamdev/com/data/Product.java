e811ee2e9cd6df2979bc69659d71cbc82d77c282
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-07 16:23:41 +0700

    Package

---
+++ src/hanifiamdev/com/data/Product.java
@@ -0,0 +1,12 @@
+package hanifiamdev.com.data;
+
+public class Product {
+
+    String name;
+    int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
