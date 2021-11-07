951c157d3c381639bece65f8c18088ec83a14688
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-07 12:58:37 +0700

    Super Constructor

--- src/Manager.java
+++ src/Manager.java
@@ -2,6 +2,11 @@ public class Manager {
 
     String name;
 
+    /*Manager() {}*/ // ini constructor default(tidak memiiliki parameter) sehingga  tidak wajib bikin constructor di kelas turunannya
+    Manager(String name) {
+        this.name = name;
+    }
+
     void sayHello(String name) {
         System.out.println("Hi " + name + ", My name Is Manager " + this.name);
     }
--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,12 +1,10 @@
 public class ManagerApp {
     public static void main(String[] args) {
 
-        var manager = new Manager();
-        manager.name = "Hanif";
+        var manager = new Manager("Hanif");
         manager.sayHello("Fachri");
 
-        var vp = new VicePresident();
-        vp.name = "Orkhan";
+        var vp = new VicePresident("Orkhan");
         vp.sayHello("Adiba");
     }
 }
