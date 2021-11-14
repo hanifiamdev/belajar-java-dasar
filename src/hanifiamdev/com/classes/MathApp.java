6364c30b7fcfec876234cbd1b8bd06fae3d5bf21
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-15 02:38:54 +0700

    Math Class

---
+++ src/hanifiamdev/com/classes/MathApp.java
@@ -0,0 +1,11 @@
+package hanifiamdev.com.classes;
+
+public class MathApp {
+    public static void main(String[] args) {
+        var min = Math.min(1000, 2000);
+        System.out.println(min);
+
+        var max = Math.max(1000, 2000);
+        System.out.println(max);
+    }
+}
