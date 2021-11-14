ca65426984aa39677c3617b1ffa510e0ec49603f
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-14 16:24:51 +0700

    StackTraceElement Class ( Mutiple StackTraceElement )

--- src/hanifiamdev/com/application/StackTraceApp.java
+++ src/hanifiamdev/com/application/StackTraceApp.java
@@ -4,14 +4,21 @@ public class StackTraceApp {
     public static void main(String[] args) {
 
         try {
+            sampleError();
+        } catch (RuntimeException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static void sampleError() {
+        try {
             String[] names = {
                     "Hanif", "Hafizah", "Orkhan"
             };
 
             System.out.println(names[100]);
         }catch (Throwable throwable) {
-            StackTraceElement[] stackTraceElements = throwable.getStackTrace();
-            throwable.printStackTrace();
+            throw new RuntimeException(throwable);
         }
     }
 }
