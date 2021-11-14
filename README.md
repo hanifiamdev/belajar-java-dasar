caff92a8c9d7a1c3a8533d1f4f233e686a72415a
hanifiamdev <hanifamrullahdev@gmail.com>
2021-11-14 17:01:24 +0700

    Try with Resource

---
+++ README.md
@@ -0,0 +1,5 @@
+#Belajar Java Dasar
+
+Ini adalah materi terkait belajar java dasar :
+
+Hanif Amrullah
---
+++ src/hanifiamdev/com/application/ReadApp.java
@@ -0,0 +1,34 @@
+package hanifiamdev.com.application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadApp {
+    public static void main(String[] args) {
+        // Versi close resouce manual
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader("README.md"));
+            while(true) {
+                String line = reader.readLine();
+                if(line == null) {
+                    break;
+                }
+                System.out.println(line);
+            }
+        } catch (Throwable throwable) {
+            System.out.println("Error membaca file " + throwable.getMessage());
+        } finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                    System.out.println("Sukses Menutup");
+                } catch (IOException exception) {
+                    System.out.println("Error menutup resouce " + exception.getMessage());
+                }
+            }
+        }
+    }
+}
